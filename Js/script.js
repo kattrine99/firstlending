d63e7912acff3ef641425bce316f37cc81a899b5
Ekaterina <kattren990703@gmail.com>
2024-09-28 21:41:24 +0500

    added pop up page

--- Js/script.js
+++ Js/script.js
@@ -39,3 +39,54 @@ sale();
 setInterval(()=>{
     sale()
 },1000);
+
+
+let wrapper = document.querySelector(".wrapper");
+let popUp = document.querySelector(".wrapper .popUp");
+let typ = document.querySelector(".wrapper .thankYouPage");
+let btns = document.querySelectorAll("section button");
+let formBtn =document.querySelector(".form button");
+let username = document.getElementsByName("username")[0];
+let userphone = document.getElementsByName("userphone")[0];
+
+
+btns.forEach(btn =>{
+    btn.addEventListener('click', showpopUp);
+});
+
+wrapper.addEventListener('click', hidepopUp);
+
+function showpopUp(){
+    wrapper.style.display = "flex";
+    popUp.style.display = "flex";
+    typ.style.display = "none";
+}
+
+function hidepopUp(){
+    let elem = event.target;
+
+    if(elem == wrapper)
+    {
+        wrapper.style.display = "none";
+    }
+}
+formBtn.addEventListener("click", sentForm);
+function sentForm(){
+    event.preventDefault();
+    if(username.value.length > 1){
+        if(userphone.value.length == 13)
+        {
+            showTYP();
+        }
+        else{
+            console.log("неверный номер");
+        }
+    }
+    else{
+        console.log("Короткое имя");
+    }
+}
+function showTYP(){
+    popUp.style.display = "none";
+    typ.style.display = "flex";
+}
